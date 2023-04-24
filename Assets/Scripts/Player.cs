1d91849038dd854121de1581bb825f2cd0c38f48
ahmads1990 <ahmads20002000@gmail.com>
2023-04-25 01:27:20 +0200

    collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,62 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+
+        bool canMove = !Physics.CapsuleCast(
+            transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius, 
+            moveDir,
+            moveDistance
+            );
+
+
+
+        if (!canMove)
+        {
+            // Cannot move toward moveDir (hug the wall)
+
+            // attempt move on x
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirX,
+                moveDistance
+            );
+            if (canMove)
+                // can move only on the x
+                moveDir = moveDirX;
+            else
+            {
+                // cannot move only on the x
+                // attempt move on z
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance
+                );
+
+                if (canMove)
+                    // can move only on the x
+                    moveDir = moveDirZ;
+                else
+                {
+                    // just cannot move in any dir
+                }
+            }
+        }
+
+        if (canMove)
+            transform.position += moveDir * moveDistance;
 
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
