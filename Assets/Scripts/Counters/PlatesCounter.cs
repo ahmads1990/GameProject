3e7c0d7a018f74c6899edbbff3199c47c7cb5172
ahmads1990 <ahmads20002000@gmail.com>
2023-04-29 06:40:21 +0300

    added Plates

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+
+    public event EventHandler OnPlateSpawned;
+
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    [SerializeField] private float spawnTimerMax = 4f;
+
+    private int platesSpawnAmount;
+    private int platesSpawnAmountMax = 4;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        if(spawnPlateTimer > spawnTimerMax)
+        {
+            spawnPlateTimer = 0f;
+            if (platesSpawnAmount < platesSpawnAmountMax)
+            {
+                platesSpawnAmount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            // player is empty handed
+            if(platesSpawnAmount > 0)
+            {
+                // there is at least one plate spawned
+                platesSpawnAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
